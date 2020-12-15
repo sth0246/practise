a208af8fdc010788e77987bc680fd8d7e07778a2
sth0246 <1137506914@qq.com>
2020-12-16 00:57:42 +0800

    轮播图优化，选项卡制作完成

--- 痛苦作业3/js.js
+++ 痛苦作业3/js.js
@@ -1,14 +1,58 @@
+var pull1 = document.getElementById("main")
+var menu1 = document.getElementById("lev2")
+var btn1 = document.getElementById("btn")
+var change = document.getElementById("view")
+var oli = document.getElementsByClassName("ali")
+var ali1 = Array.from(oli)
+var aauto = document.getElementsByClassName("auto")
+var aauto1 = Array.from(aauto)
+var aau = document.getElementsByClassName("au")
+var aau1 = Array.from(aau)
 var i = 0
 var num = 0
-var oli = 
-var change = document.getElementById("view")
-function changeImg(){
-    i+=1581
-    change.style.left = "-"+ i + "px"
-    oli[num].
-    num++
-    if(i == 6324){
-        i = 0
+
+
+for(let m = 0;m<aauto1.length;m++){
+    aauto1[m].index = m
+    aauto1[m].onclick = function(){
+        for(let j =0;j < aauto1.length;j++){
+            aauto1[j].className = 'auto';
+            aau1[j].className = 'au';
+        }
+        this.className = 'autox';
+        aau1[aauto1[m].index].className = 'aux'
     }
 }
-setInterval("changeImg()",1000)
+ali1[0].className="active"      
+function changeImg() {
+    if(num==0){
+        ali1[4].className="ali"
+    }
+    if(num>=1){
+        ali1[num-1].className="ali"
+    }
+    
+    change.style.left = "-" + i + "px"
+    ali1[num].className="active"
+    num++
+    i += 1581
+    if (i > 6324) {
+        i = 0
+    }
+    if (num==5){
+        num=0
+    }
+   
+  
+}
+setInterval("changeImg()", 1000)
+function menu() {
+        menu1.style.display = "block"
+    }
+function menua() {
+        menu1.style.display = "none"
+    }
+function pull() {
+        pull1.style.height = 1660 + "px"
+        btn1.style.display = "none"
+    }
